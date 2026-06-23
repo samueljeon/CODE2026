73950698595023d12e5b49bf9d66c2cabcee0016
Mars <marsshi13@gmail.com>
2026-06-23 07:35:48 -0700

    Refactor ROV code for distance sensor and motor control
    Removed unused libraries and variables, added distance sensor functionality, and improved motor control logic.

--- MATE ROV CODE/ROV CODE/MATE_ROV_NEWARM_AARON/ROV/NEWARM_DC_01/NEWARM_DC_01.ino
+++ MATE ROV CODE/ROV CODE/MATE_ROV_NEWARM_AARON/ROV/NEWARM_DC_01/NEWARM_DC_01.ino
@@ -1,24 +1,18 @@
 #include <DHT.h>
 #include <Servo.h>
-#include <Wire.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 
 #define DHTPIN 47
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
-Adafruit_MPU6050 mpu;
-
-//configs-change if needed
-int elbow_default_pos = 1800; 
-//1500 us is default servo pos; however, due to the installation the default pos makes the elbow boot to a depressed angle, damaging servo. 
-//1800 ms makes elbow actually level on boot
 
 const int thrusterMin = 1100;
 const int thrusterMax = 1900;
 const int rotateMin   = 500;
 const int rotateMax   = 2500;
+
+#define DistanceSerial Serial3
+const uint8_t DIST_TRIGGER_CMD = 0x55;
 
 enum InputSignalRange {
   RANGE_NEG_ONE_TO_ONE,
@@ -40,16 +34,9 @@ struct ROVPacket {
 
 struct TelemetryPacket {
   uint8_t start = 0xAB;
-
-  float accel_x_g;
-  float accel_y_g;
-  float accel_z_g;
-
-  float pitch_deg;
-  float roll_deg;
-
-  float temp;
-
+  float temperature;
+  float humidity;
+  int distanceMm;
   uint8_t end = 0x54;
 };
 #pragma pack(pop)
@@ -57,7 +44,6 @@ struct TelemetryPacket {
 const uint8_t START_BYTE = 0xAA;
 const uint8_t END_BYTE   = 0x55;
 
-// --- Thrusters & elbow servo ---
 Servo servo_lf;
 Servo servo_rt;
 Servo servo_up1;
@@ -70,17 +56,17 @@ const byte servoPin_up1    = 28;
 const byte servoPin_up2    = 22;
 const byte servoPin_rotate = 23;
 
-// --- H-bridge pins ---
-// DC Motor 1
 const byte M1_IN1 = 4;
 const byte M1_IN2 = 5;
 const byte M1_EN  = 9;
 
-// DC Motor 2
 const byte M2_IN1 = 6;
 const byte M2_IN2 = 7;
 const byte M2_EN  = 10;
 
+const byte MOTOR_SPEED = 128;
+
+int latestDistanceMm = -1;
 
 int mapFloatSignal(float input, int minVal, int maxVal, InputSignalRange rangeType) {
   input = constrain(input, (rangeType == RANGE_NEG_ONE_TO_ONE ? -1.0f : 0.0f), 1.0f);
@@ -95,105 +81,87 @@ int mapFloatSignal(float input, int minVal, int maxVal, InputSignalRange rangeTy
 }
 
 void driveMotor(byte in1, byte in2, byte en, float value) {
-  value = constrain(value, -1.0f, 1.0f);
-
-  int pwm = abs(value) * 255;
-
-  if (value > 0.1f) {
+  if (value > 0.01f) {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
-    analogWrite(en, pwm);
-  } 
-  else if (value < -0.1f) {
+    analogWrite(en, MOTOR_SPEED);
+  } else if (value < -0.1f) {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
-    analogWrite(en, pwm);
-  } 
-  else {
+    analogWrite(en, MOTOR_SPEED);
+  } else {
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
     analogWrite(en, 0);
   }
 }
 
-
-//------------------------------------------------------------
 void applyControls(const ROVPacket& p) {
-  servo_lf.writeMicroseconds(mapFloatSignal(p.tleft,   thrusterMin, thrusterMax, RANGE_NEG_ONE_TO_ONE));
+  servo_lf.writeMicroseconds(mapFloatSignal(p.tleft, thrusterMin, thrusterMax, RANGE_NEG_ONE_TO_ONE));
   servo_rt.writeMicroseconds(mapFloatSignal(-p.tright, thrusterMin, thrusterMax, RANGE_NEG_ONE_TO_ONE));
 
-  servo_up1.writeMicroseconds(mapFloatSignal(p.tup_left,  thrusterMin, thrusterMax, RANGE_NEG_ONE_TO_ONE));
+  servo_up1.writeMicroseconds(mapFloatSignal(p.tup_left, thrusterMin, thrusterMax, RANGE_NEG_ONE_TO_ONE));
   servo_up2.writeMicroseconds(mapFloatSignal(p.tup_right, thrusterMin, thrusterMax, RANGE_NEG_ONE_TO_ONE));
 
   servo_rotate.writeMicroseconds(mapFloatSignal(p.rotate, rotateMin, rotateMax, RANGE_ZERO_TO_ONE));
 
-  //motor 1 (linear actuator)(blue tape/white/green cable)
   driveMotor(M1_IN1, M1_IN2, M1_EN, p.dc_motor);
-  //motor 2
   driveMotor(M2_IN1, M2_IN2, M2_EN, p.dc_motor2);
 }
 
+void requestDistanceSensor() {
+  while (DistanceSerial.available()) {
+    DistanceSerial.read();
+  }
+
+  DistanceSerial.write(DIST_TRIGGER_CMD);
+}
+
+void readDistanceSensor() {
+  while (DistanceSerial.available() >= 4) {
+    if (DistanceSerial.peek() != 0xFF) {
+      DistanceSerial.read();
+      continue;
+    }
+
+    uint8_t header = DistanceSerial.read();
+    uint8_t highByte = DistanceSerial.read();
+    uint8_t lowByte = DistanceSerial.read();
+    uint8_t checksum = DistanceSerial.read();
+
+    uint8_t calculatedChecksum = header + highByte + lowByte;
+
+    if (calculatedChecksum == checksum) {
+      latestDistanceMm = ((int)highByte << 8) | lowByte;
+    }
+  }
+}
+
 void sendTelemetry() {
-  float dhtTemperature = dht.readTemperature();
-  if (isnan(dhtTemperature)) dhtTemperature = -99.0f;
+  float temperature = dht.readTemperature();
+  if (isnan(temperature)) temperature = -99.0;
 
   float humidity = dht.readHumidity();
-  if (isnan(humidity)) humidity = -99.0f;
-
-  sensors_event_t accelEvent;
-  sensors_event_t gyroEvent;
-  sensors_event_t mpuTempEvent;
-
-  mpu.getEvent(&accelEvent, &gyroEvent, &mpuTempEvent);
-
-  const float G = 9.80665f;
-
-  float accelX = accelEvent.acceleration.x / G;
-  float accelY = accelEvent.acceleration.y / G;
-  float accelZ = accelEvent.acceleration.z / G;
-
-  float roll = atan2(accelY, accelZ) * 180.0f / PI;
-
-  float pitch = atan2(
-    -accelX,
-    sqrt(accelY * accelY + accelZ * accelZ)
-  ) * 180.0f / PI;
-
-  float mpuTempC = mpuTempEvent.temperature;
+  if (isnan(humidity)) humidity = -99.0;
 
   TelemetryPacket t;
-
-  t.accel_x_g = accelX;
-  t.accel_y_g = accelY;
-  t.accel_z_g = (accelZ-0.115f);  //Default shows Z-accel of 1.115g, so I calibrated it
-
-  t.pitch_deg = pitch;
-  t.roll_deg = roll;
-
-  // This sends MPU6050 internal temperature.
-  // If you want DHT22 temperature instead, change this to: t.temp = dhtTemperature;
-  t.temp = mpuTempC;
+  t.temperature = temperature;
+  t.humidity = humidity;
+  t.distanceMm = latestDistanceMm;
 
   Serial.write((uint8_t*)&t, sizeof(t));
   Serial.flush();
 }
 
 void setup() {
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  digitalWrite(10, HIGH);
+
   Serial.begin(115200);
+  DistanceSerial.begin(115200);
+
   dht.begin();
-
-  Wire.begin();
-
-  if (!mpu.begin()) {
-    Serial.println("MPU6050 not found!");
-    while (1) {
-      delay(10);
-    }
-  }
-
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(44, OUTPUT);
@@ -208,30 +176,34 @@ void setup() {
   servo_rt.writeMicroseconds((thrusterMin + thrusterMax) / 2);
   servo_up1.writeMicroseconds((thrusterMin + thrusterMax) / 2);
   servo_up2.writeMicroseconds((thrusterMin + thrusterMax) / 2);
-  servo_rotate.writeMicroseconds(elbow_default_pos); 
+  servo_rotate.writeMicroseconds((rotateMin + rotateMax) / 2);
 
-  pinMode(M1_IN1, OUTPUT);
-  pinMode(M1_IN2, OUTPUT);
-  pinMode(M2_IN1, OUTPUT);
-  pinMode(M2_IN2, OUTPUT);
+  pinMode(M1_IN1, OUTPUT); pinMode(M1_IN2, OUTPUT);
+  pinMode(M2_IN1, OUTPUT); pinMode(M2_IN2, OUTPUT);
 
-  pinMode(M1_EN, OUTPUT);
-  pinMode(M2_EN, OUTPUT);
+  pinMode(M1_EN, OUTPUT); analogWrite(M1_EN, MOTOR_SPEED);
+  pinMode(M2_EN, OUTPUT); analogWrite(M2_EN, MOTOR_SPEED);
 
-  analogWrite(M1_EN, 0);
-  analogWrite(M2_EN, 0);
-
-  digitalWrite(M1_IN1, LOW);
-  digitalWrite(M1_IN2, LOW);
-  digitalWrite(M2_IN1, LOW);
-  digitalWrite(M2_IN2, LOW);
+  digitalWrite(M1_IN1, LOW); digitalWrite(M1_IN2, LOW);
+  digitalWrite(M2_IN1, LOW); digitalWrite(M2_IN2, LOW);
 }
 
 void loop() {
   static uint8_t buffer[sizeof(ROVPacket)];
   static size_t index = 0;
+
   static unsigned long lastTelemetryTime = 0;
-  const unsigned long telemetryInterval = 5;
+  static unsigned long lastDistanceRequestTime = 0;
+
+  const unsigned long telemetryInterval = 500;
+  const unsigned long distanceInterval = 200;
+
+  readDistanceSensor();
+
+  if (millis() - lastDistanceRequestTime >= distanceInterval) {
+    requestDistanceSensor();
+    lastDistanceRequestTime = millis();
+  }
 
   if (millis() - lastTelemetryTime >= telemetryInterval) {
     sendTelemetry();
@@ -239,13 +211,13 @@ void loop() {
   }
 
   while (Serial.available()) {
-    uint8_t incomingByte = Serial.read();
+    uint8_t byte = Serial.read();
 
-    if (index == 0 && incomingByte != START_BYTE) {
+    if (index == 0 && byte != START_BYTE) {
       continue;
     }
 
-    buffer[index++] = incomingByte;
+    buffer[index++] = byte;
 
     if (index == sizeof(ROVPacket)) {
       ROVPacket pkt;
